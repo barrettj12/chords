10f717e376032aee254dd8c2140fce01b18440a5
Jordan Barrett <jordan.barrett@canonical.com>
2022-11-16 21:37:44 +1100

    Album/TrackNum omitempty

--- src/dblayer/db.go
+++ src/dblayer/db.go
@@ -49,8 +49,8 @@ type SongMeta struct {
 	ID       string `json:"id"`
 	Name     string `json:"name"`
 	Artist   string `json:"artist"`
-	Album    string `json:"album"`
-	TrackNum int    `json:"trackNum"`
+	Album    string `json:"album,omitempty"`
+	TrackNum int    `json:"trackNum,omitempty"`
 }
 
 type Chords []byte
