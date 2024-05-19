de2daee13447d473cbe02f71a3292523764c2f99
Jordan Barrett <90195985+barrettj12@users.noreply.github.com.>
2024-05-19 18:27:54 +0200

    Artists appear in search results - new type types.SearchResult - minor change to ChordsDB interface

--- src/types/types.go
+++ src/types/types.go
@@ -7,3 +7,13 @@ type SongMeta struct {
 	Album    string `json:"album,omitempty"`
 	TrackNum int    `json:"trackNum,omitempty"`
 }
+
+type SearchResult struct {
+	Type SearchResultType `json:"type"` // "song" or "artist"
+
+	Name string    `json:"name,omitempty"` // populated for type:artist only
+	ID   string    `json:"id,omitempty"`   // populated for type:song only
+	Meta *SongMeta `json:"meta,omitempty"` // populated for type:song only
+}
+
+type SearchResultType string
